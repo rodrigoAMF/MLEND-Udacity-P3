faf97d646d811779d70c4765b5390277699954ea
Rodrigo Franco <rodrigoamfranco@gmail.com>
2022-07-31 15:49:06 -0300

    Add 2 new unit tests to test_model

--- src/test_model.py
+++ src/test_model.py
@@ -1,7 +1,7 @@
 import lightgbm as lgb
 import numpy as np
 
-from lib.ml.model import train_model
+from lib.ml.model import train_model, inference, compute_model_metrics
 
 
 def test_train_model():
@@ -12,3 +12,34 @@ def test_train_model():
 
     assert model is not None
     assert isinstance(model, lgb.LGBMClassifier)
+
+
+def test_inference():
+    X_train = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    X_test = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    y_train = np.array([0, 1, 0])
+
+    model = train_model(X_train, y_train)
+
+    preds = inference(model, X_test)
+
+    assert preds is not None
+    assert isinstance(preds, np.ndarray)
+    assert preds.shape == (3,)
+
+
+def test_compute_metrics():
+    X_train = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    X_test = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    y_train = np.array([0, 1, 0])
+    y_test = np.array([0, 0, 1])
+
+    model = train_model(X_train, y_train)
+    preds = inference(model, X_test)
+    metrics = compute_model_metrics(y_test, preds)
+
+    assert metrics is not None
+    assert len(metrics) == 3
+    assert isinstance(metrics[0], float)    # precision
+    assert isinstance(metrics[1], float)    # recall
+    assert isinstance(metrics[2], float)    # fbeta
